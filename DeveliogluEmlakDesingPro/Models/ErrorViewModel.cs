e24513ec11523208e3e82d53d32ad05b0da3ba14
Hamza Caylan <hamzacaylan021@gmail.com>
2024-10-12 19:15:56 +0300

    Proje dosyası ekle.

---
+++ DeveliogluEmlakDesingPro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DeveliogluEmlakDesingPro.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
---
+++ EntityLayer/Concrete/Block.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+	public  class Block
+	{
+		//emlak ilan liste elemanlarini iceriyor
+		public int BlockID { get; set; }
+
+		public string BlockName { get; set; }
+
+		public string Bloccontent { get; set; }
+
+		public string BlockCreateDate {get; set; }
+
+	}
+}
