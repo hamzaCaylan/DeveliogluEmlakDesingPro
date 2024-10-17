d78e783f4fbf3130d84ee2c0bb7c145be8393eed
Hamza Caylan <hamzacaylan021@gmail.com>
2024-10-18 00:02:48 +0300

    efRepository ve ...Dal eklendi.

---
+++ BusinesLayer/Concrete/EmlakManager.cs
@@ -0,0 +1,47 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinesLayer.Concrete
+{
+	public class EmlakManager : IEmlakDal
+	{
+		IEmlakDal _emlakDal;
+
+		public EmlakManager(IEmlakDal emlakDal)
+		{
+			_emlakDal = emlakDal;
+		}		
+		
+
+		public void Delete(Emlak t)
+		{
+			_emlakDal.Delete(t);
+		}
+
+		public Emlak GetByID(int id)
+		{
+			return _emlakDal.GetByID(id);
+		}
+
+		public List<Emlak> GetListAll()
+		{
+			return _emlakDal.GetListAll();
+		}
+
+		public void Insert(Emlak t)
+		{
+			_emlakDal.Insert(t);
+		}
+
+		public void Update(Emlak t)
+		{
+			_emlakDal.Update(t);
+		}
+	}
+}
