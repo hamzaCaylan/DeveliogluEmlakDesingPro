5de3175536bb5566a4a7b9b9a6f355cab9231f1f
Hamza Caylan <hamzacaylan021@gmail.com>
2024-12-18 10:52:21 +0300

    detay ViewComponetns eklendi migrationIamge

---
+++ DataAccessLayer/Migrations/20241218074813_mig_Image.cs
@@ -0,0 +1,165 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_Image : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "EmlakYayinda",
+                table: "Emlaks",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmlakOtopark",
+                table: "Emlaks",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmlakEsya",
+                table: "Emlaks",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "EmlakAsansor",
+                table: "Emlaks",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Cephe",
+                table: "Emlaks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Fiyat",
+                table: "Emlaks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "KonutYas",
+                table: "Emlaks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Kredi",
+                table: "Emlaks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TapuDurum",
+                table: "Emlaks",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Image",
+                columns: table => new
+                {
+                    ImageID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImageName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmlakID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Image", x => x.ImageID);
+                    table.ForeignKey(
+                        name: "FK_Image_Emlaks_EmlakID",
+                        column: x => x.EmlakID,
+                        principalTable: "Emlaks",
+                        principalColumn: "EmlakID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Image_EmlakID",
+                table: "Image",
+                column: "EmlakID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Image");
+
+            migrationBuilder.DropColumn(
+                name: "Cephe",
+                table: "Emlaks");
+
+            migrationBuilder.DropColumn(
+                name: "Fiyat",
+                table: "Emlaks");
+
+            migrationBuilder.DropColumn(
+                name: "KonutYas",
+                table: "Emlaks");
+
+            migrationBuilder.DropColumn(
+                name: "Kredi",
+                table: "Emlaks");
+
+            migrationBuilder.DropColumn(
+                name: "TapuDurum",
+                table: "Emlaks");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "EmlakYayinda",
+                table: "Emlaks",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "EmlakOtopark",
+                table: "Emlaks",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "EmlakEsya",
+                table: "Emlaks",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "EmlakAsansor",
+                table: "Emlaks",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
--- EntityLayer/Concrete/Emlak.cs
+++ EntityLayer/Concrete/Emlak.cs
@@ -20,35 +20,50 @@ namespace EntityLayer.Concrete
 
 		public int EmlakKat { get; set; }
 
-		public bool EmlakAsansor {  get; set; }
+		public string EmlakAsansor {  get; set; }
 
-		public bool EmlakOtopark {  get; set; }
+		public string EmlakOtopark {  get; set; }
 
 		public string EmlakIsitma {  get; set; }
 
-		public bool EmlakEsya{get; set; }
+		public string EmlakEsya {get; set; }
 
-		public bool EmlakYayinda {  get; set; }
+		public string EmlakYayinda {  get; set; }
 
 		public string EmlakAciklama {  get; set; }
 
-        public int BolgeMahalleID { get; set; }
+		public string Fiyat { get; set; }
+
+		public string Kredi { get; set; }
+
+		public string TapuDurum { get; set; }
+
+		public string KonutYas { get; set; }
+
+		public string Cephe { get; set; }
+
+
+		public int BolgeMahalleID { get; set; }
 
         public BolgeMahalle BolgeMahalles { get; set; }
+
 
         public int YoneticiID { get; set; }
 
         public Yonetici Yoneticis { get; set; }
 
+
         public int EmlakTurID { get; set; }
 
         public EmlakTur EmlakTurs { get; set; }
 
+
         public int EmlakTipID { get; set; }
 
         public EmlakTip EmlakTips { get; set; }
-        
+
+		public List<Image> Images { get; set; }
 
 
-    }
+	}
 }
